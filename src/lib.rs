104008ac593d3a23ea310e1f00a1586cc8d3b7a2
ale <malejlopez@hotmail.com>
2024-01-17 14:09:34 -0400

    Finished implementaion for return types in the JoinHandle

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,53 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, ItemFn};
+use syn::{parse_macro_input, parse_quote_spanned, spanned::Spanned, ItemFn, ReturnType};
 
 #[proc_macro_attribute]
 pub fn background_task(_attr: TokenStream, tokens: TokenStream) -> TokenStream {
     let mut item = parse_macro_input!(tokens as ItemFn);
 
+    // Change `async fn()` to `fn()`
     item.sig.asyncness = None;
-
     let ItemFn {
         attrs,
         vis,
-        sig,
+        mut sig,
         block,
     } = item;
 
-    let stmts = &block.stmts;
-
-    let new = quote! {
-        #(#attrs)* #vis #sig {
-            tokio::spawn(async {
-                #(#stmts)*
-            });
+    // Change fn
+    match &sig.output {
+        ReturnType::Default => {}
+        ReturnType::Type(_, ret) => {
+            let ret = quote!(#ret);
+            sig.output = parse_quote_spanned! {ret.span()=>
+                -> tokio::task::JoinHandle<#ret>
+            };
         }
     };
 
-    new.into()
+    let stmts = &block.stmts;
+
+    let new_tokens = match &sig.output {
+        ReturnType::Default => {
+            quote! {
+                #(#attrs)* #vis #sig {
+                    tokio::spawn(async {
+                        #(#stmts)*
+                    });
+                }
+            }
+        }
+        ReturnType::Type(_, _) => {
+            quote! {
+                #(#attrs)* #vis #sig {
+                    tokio::spawn(async {
+                        #(#stmts)*
+                    })
+                }
+            }
+        }
+    };
+
+    new_tokens.into()
 }
