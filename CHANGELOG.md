e83b8175b2e7cd05dc7b79aa71043b70cfd9cb39
ale <malejlopez@hotmail.com>
2024-01-17 11:58:21 -0400

    Finished background_task macros for fn to run at backgroun as a tokio task

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# 0.1.0 (January 17, 2024)
+
+- Initial release
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,29 @@
 use proc_macro::TokenStream;
 use quote::{quote, ToTokens};
-use syn::{parse_macro_input, ItemFn, StmtMacro};
+use syn::{parse_macro_input, ItemFn};
 
 #[proc_macro_attribute]
-pub fn task_spawn(_attr: TokenStream, tokens: TokenStream) -> TokenStream {
-    // Construct a representation of Rust code as a syntax tree
-    // that we can manipulate
-    let old_tokens = tokens.clone();
+pub fn background_task(_attr: TokenStream, tokens: TokenStream) -> TokenStream {
     let mut item = parse_macro_input!(tokens as ItemFn);
 
-    item.block
-        .stmts
-        .insert(0, syn::parse(quote!(println!("Start");).into()).unwrap());
+    item.sig.asyncness = None;
 
-    item.to_token_stream().into()
+    let ItemFn {
+        attrs,
+        vis,
+        sig,
+        block,
+    } = item;
+
+    let stmts = &block.stmts;
+
+    let new = quote! {
+        #(#attrs)* #vis #sig {
+            tokio::spawn(async {
+                #(#stmts)*
+            });
+        }
+    };
+
+    new.into()
 }
